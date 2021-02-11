8e003340e98a83004ddec6dbf9a6200a4e6a31e1
marti <marti@192.168.1.40>
2021-02-11 21:04:27 +0100

    2. Creación del enumerado Curso.

--- src/test/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/dominio/CursoTest.java
+++ src/test/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/dominio/CursoTest.java
@@ -1,6 +1,9 @@
 package org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio;
 
 import static org.junit.Assert.*;
+
+import org.iesalandalus.programacion.biblioteca.mvc.dominio.Curso;
+
 import static org.hamcrest.CoreMatchers.is;
 
 
